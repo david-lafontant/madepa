1ff026569f2a70c6d4a869f4ba19655f39fb9941
david-lafontant <lafontant.david@gmail.com>
2022-08-31 11:28:17 -0600

    Remove button and fix card height in services

--- src/components/services/services.jsx
+++ src/components/services/services.jsx
@@ -3,16 +3,15 @@ import React from 'react';
 function Services() {
   return (
     <section>
-      <div className="hero min-h-screen bg-base-200">
+      <div className="hero min-h-fit bg-base-200">
         <div className="hero-content text-center">
           <div className="max-w-md">
-            <h1 className="text-5xl font-bold">Hello there</h1>
+            <h1 className="text-5xl font-bold">SERVICES</h1>
             <p className="py-6">
               Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
               excepturi exercitationem quasi. In deleniti eaque aut repudiandae
               et a id nisi.
             </p>
-            <button type="button" className="btn btn-primary">Get Started</button>
           </div>
         </div>
       </div>
@@ -24,13 +23,12 @@ function Services() {
             alt="service 1"
           />
           <div>
-            <h1 className="text-5xl font-bold">Box Office News!</h1>
+            <h1 className="text-5xl font-bold">SERVICE 1</h1>
             <p className="py-6">
               Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
               excepturi exercitationem quasi. In deleniti eaque aut repudiandae
               et a id nisi.
             </p>
-            <button className="btn btn-primary">Get Started</button>
           </div>
         </div>
       </div>
@@ -42,13 +40,29 @@ function Services() {
             alt="service 2"
           />
           <div>
-            <h1 className="text-5xl font-bold">Box Office News!</h1>
+            <h1 className="text-5xl font-bold">SERVICE 2</h1>
             <p className="py-6">
               Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
               excepturi exercitationem quasi. In deleniti eaque aut repudiandae
               et a id nisi.
             </p>
-            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row">
+          <img
+            src="https://placeimg.com/260/400/arch"
+            className="max-w-sm rounded-lg shadow-2xl"
+            alt="service 1"
+          />
+          <div>
+            <h1 className="text-5xl font-bold">SERVICE 3</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
           </div>
         </div>
       </div>
