4b76c0c880073417530f0eba7ca7fddd61b47e03
david-lafontant <lafontant.david@gmail.com>
2022-08-31 11:26:16 -0600

    Fix background image displaying

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
+import img1 from '../../assets/images/hygienne.png';
 
 const myComponentStyle = {
-  backgroundImage: "url(https://placeimg.com/1000/800/arch)"
-}
-
+  backgroundImage: `url(${img1})`,
+};
 
 function Home() {
   return (
-    <div className="hero min-h-screen" style={myComponentStyle}>
+    <div id="home" className="hero min-h-screen" style={myComponentStyle}>
       <div className="hero-overlay bg-opacity-60"></div>
       <div className="hero-content text-center text-neutral-content">
         <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+          <h1 className="mb-5 text-5xl font-bold">MADEPA</h1>
           <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
           <button type="button" className="btn btn-primary">Get Started</button>
         </div>
