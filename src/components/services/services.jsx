a68eba353393ff16e51a592d1c53b182cba40f5c
david-lafontant <lafontant.david@gmail.com>
2022-08-31 05:41:18 -0600

    Setup tailwindCSS

--- src/components/services/services.jsx
+++ src/components/services/services.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Services() {
   return (
-    <div>services</div>
+    <div>services
+        <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    </div>
   )
 }
 
