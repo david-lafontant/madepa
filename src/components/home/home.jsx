671e79620549be1dac19cedf7044d9d2b2a76eb5
david-lafontant <lafontant.david@gmail.com>
2022-08-31 10:10:11 -0600

    Update home component

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+
+const myComponentStyle = {
+  backgroundImage: "url(https://placeimg.com/1000/800/arch)"
+}
+
 
 function Home() {
   return (
-    <div>home</div>
-  )
+    <div className="hero min-h-screen" style={myComponentStyle}>
+      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-content text-center text-neutral-content">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+          <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+          <button type="button" className="btn btn-primary">Get Started</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
